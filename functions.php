018b2bf235eba251151ef2ee08ce778c3c5bde21
Brent Jett <brentjett@gmail.com>
2015-05-15 10:34:29 -0500

    update w/ comments

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 
 // if the config.json file is somewhere other than in the root of the theme, define BASSET_CONFIG_PATH with the path including the filename before including the theme_config_lib index file.
 
-require_once 'includes/required_plugins/index.php';
-require_once 'includes/updater/index.php';
-require_once 'includes/less/index.php';
-require_once 'includes/theme_config_lib/index.php';
-require_once 'includes/acf/index.php';
-require_once 'includes/components/index.php';
-require_once 'includes/templates/index.php';
+require_once 'includes/required_plugins/index.php'; // Require Plugins using TGM library
+require_once 'includes/updater/index.php'; // WP-Updates.com updates
+require_once 'includes/less/index.php'; // Less Compiler
+require_once 'includes/theme_config_lib/index.php'; // Read theme config files
+require_once 'includes/acf/index.php'; // Extend Advanced Custom Fields Plugin
+require_once 'includes/components/index.php'; // Shortcodes
+require_once 'includes/templates/index.php'; // Template script/style enqueue library
 
 
 // Disable Theme & Plugin Editors - Child themes can re-enable it by setting this constant to false
@@ -23,19 +23,6 @@ add_action('wp_enqueue_scripts', function() {
 
 add_action('after_setup_theme', function() {
 	add_theme_support('title-tag'); // Support <title> by default
-
-	/* NEED TO TEST THIS
-	if (!function_exists('get_field')) {
-		function get_field($key, $type_handle = '', $default_value = '') {
-			if ($type_handle == 'option' || $type_handle == 'options') {
-				return get_option($key, $default);
-			} else {
-				global $post;
-				return get_post_meta($post->ID, $key);
-			}
-		}
-	}
-	*/
 });
 add_action('admin_init', function() {
 	add_editor_style('libraries/editor.less');
@@ -54,11 +41,5 @@ function basset_get($key, $type_handle = '', $default_value = '') {
 			return get_post_meta($post->ID, $key);
 		}
 	}
-}
-
-function basset_nav_menu_fallback($args) {
-	?>
-	<div class="nav-menu-placeholder"><a href="/wp-admin/nav-menus.php">Assign Nav Menu to <?=$args['location']?></a></div>
-	<?
 }
 ?>
